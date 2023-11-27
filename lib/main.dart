b9eff8ee91a7bbd2ee4c02a8c49acd750b4c46b5
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-27 11:52:45 +0530

    different audio files added corresponding to the buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,118 @@
 import 'package:flutter/material.dart';
+
 import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(child: Text(nouns.last)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      )),
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.yellow,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      )),
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      )),
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      )),
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.indigo,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      )),
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.purple.shade900,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      )),
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: Text(''),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
